abd2be27bc7377baab1963b9c995046554a9fd45
Logic <38597904+LogicFan@users.noreply.github.com>
2024-06-26 23:19:32 -0400

    rename file

--- src/user_interface/mod.rs
+++ src/user_interface/mod.rs
@@ -1,29 +1,29 @@
 use bevy::prelude::*;
 
 mod cursor;
-mod main_camera;
+mod primary_camera;
 mod parsed_input;
 mod settings;
 
 pub use cursor::lock_cursor;
 pub use cursor::release_cursor;
-pub use main_camera::spawn_main_camera;
-pub use main_camera::PrimaryCamera;
-pub use main_camera::PrimaryCameraSystemSet;
+pub use primary_camera::spawn_main_camera;
+pub use primary_camera::PrimaryCamera;
+pub use primary_camera::PrimaryCameraSystemSet;
 
 pub struct UserInterfacePlugin;
 
 impl Plugin for UserInterfacePlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(settings::InputSettings::default())
-            .insert_resource(main_camera::PrimaryCameraConstraint::default())
-            .add_systems(Startup, main_camera::spawn_main_camera)
+            .insert_resource(primary_camera::PrimaryCameraConstraint::default())
+            .add_systems(Startup, primary_camera::spawn_main_camera)
             .add_systems(
                 PostUpdate,
                 (
-                    main_camera::move_main_camera,
-                    main_camera::zoom_main_camera,
-                    main_camera::rotate_main_camera,
+                    primary_camera::move_main_camera,
+                    primary_camera::zoom_main_camera,
+                    primary_camera::rotate_main_camera,
                 )
                     .in_set(PrimaryCameraSystemSet),
             );
