c66096a8ec78d25d155e8b8aa93dea57401f86df
Logic <38597904+LogicFan@users.noreply.github.com>
2024-07-01 13:22:29 -0400

    first try with sickle_ui

--- src/user_interface/menu/background.rs
+++ src/user_interface/menu/background.rs
@@ -1,5 +1,6 @@
 use crate::user_interface::PrimaryCamera;
 use bevy::prelude::*;
+use sickle_ui::prelude::*;
 
 /// A marker component for background image.
 #[derive(Component, Clone, Copy, Debug, Default)]
@@ -17,27 +18,24 @@ pub fn spawn_menu_background(
     let image = assets.load("background/background_0.png");
     let camera = q_camera.single();
 
-    commands.spawn((
-        MenuBackground,
-        TargetCamera(camera),
-        ImageBundle {
+    commands
+        .ui_builder(UiRoot)
+        .spawn(ImageBundle {
             image: UiImage {
                 texture: image,
                 ..default()
             },
-            style: Style {
-                align_self: AlignSelf::Center,
-                justify_self: JustifySelf::Center,
-                min_width: Val::Vw(100.0),
-                min_height: Val::Vh(100.0),
-                max_width: Val::Vh(ASPECT_RATIO * 100.0),
-                max_height: Val::Vw(ASPECT_RATIO.recip() * 100.0),
-                aspect_ratio: Some(ASPECT_RATIO),
-                ..default()
-            },
             ..default()
-        },
-    ));
+        })
+        .insert((TargetCamera(camera), MenuBackground))
+        .style()
+        .align_self(AlignSelf::Center)
+        .justify_self(JustifySelf::Center)
+        .min_height(Val::Vh(100.0))
+        .max_height(Val::Vw(ASPECT_RATIO.recip() * 100.0))
+        .min_width(Val::Vw(100.0))
+        .max_width(Val::Vh(ASPECT_RATIO * 100.0))
+        .aspect_ratio(Some(ASPECT_RATIO));
 }
 
 /// Despawn the background for Menu.
